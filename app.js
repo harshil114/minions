ddb2f574d2e25fa310759e6b07fc71a8afd3c4f5
harshil114 <harshilvispute114@gmail.com>
2023-11-21 16:57:17 +0530

    fix-a bug fix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var btn = document.querySelector(".btn");
-var txtInput = document.querySelector("#txt-input");
+var btn = document.querySelector("#btn");
+var txtInput = document.querySelector("#input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL ="https://api.funtranslations.com/translate/pirate.json"
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
 
 
 function getTranslationURL(input) {
